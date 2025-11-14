98023805a55e92c2ee1f933dfa986bc8cf9cc7c2
RBsahu <rb_sahu6@yahoo.com>
2025-11-14 13:48:06 +0530

    Added input string and code alignment

--- String_Program/Count_Letters.cpp
+++ String_Program/Count_Letters.cpp
@@ -4,13 +4,13 @@
 #include <iostream>
 using namespace std;
 
-void count(string str)
+void count_Letters(string str)
 {
 	int upper = 0;
 	int lower = 0;
 	int special = 0;
 	int number = 0;
-	for (int i = 0; i < str.length(); i++) 
+	for (int i = 0; i < str.length(); i++)
 	{
 		if (isupper(str[i]))
 			upper++;
@@ -21,16 +21,18 @@ void count(string str)
 		else
 			special++;
 	}
-	cout << "Uppercase letters: " << upper << endl;
-	cout << "Lowercase letters: " << lower << endl;
-	cout << "Numeric digits: " << number << endl;
-	cout << "Special characters: " << special << endl;
+	cout << "Uppercase letters	: " << upper << endl;
+	cout << "Lowercase letters	: " << lower << endl;
+	cout << "Numeric digits		: " << number << endl;
+	cout << "Special characters	: " << special << endl;
 }
 
 int main()
 {
-	string str = "#Ram@Balak#Sahu07";
-	count(str);
+	string str = "#Ram$Balak#Sahu*15";
+	cout << "Input String		: " << str << endl;
+	count_Letters(str);
 
 	return 0;
 }
+
